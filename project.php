b7585f17938a58f48dece0ac3bf522fff02fae7b
UBC Student <lgy123@students.cs.ubc.ca>
2021-03-24 21:31:49 +0800

    UPDATE
    UPDATE by Allen

--- project.php
+++ project.php
@@ -27,6 +27,22 @@
             <input type="submit" value="Delete Movie" name="deleteSubmit">
         </form>
 
+        <hr/>
+
+        <h2>Update Customer Information in Customer List</h2>
+        <form method="POST" action="handleUpdate.php"> <!--refresh page when submitted-->
+            <input type="hidden" id="updateQueryRequest" name="updateQueryRequest">
+            CustomerID: <input type="text" name="id"> <br /><br />
+            CustomerEmail: <input type="text" name="email"> <br /><br />
+            CustomerPhone: <input type="text" name="phone"> <br /><br />
+            CustomerName: <input type="text" name="name"> <br /><br />
+            CustomerStaffID: <input type="text" name="staffId"> <br /><br />
+
+        <input type="submit" value="Update" name="updateSubmit"></p>
+        </form>
+
+        <hr/>
+
 
         <!-- <h2>Show all the Movies in List</h2>
         <form method="GET" action="handlerinsert.php">
